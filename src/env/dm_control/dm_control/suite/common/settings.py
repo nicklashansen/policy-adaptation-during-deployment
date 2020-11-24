18c226eff12e72fc0d07d0be44f33c7300f4023e
Nicklas Hansen <hello@nicklashansen.com>
2020-11-24 23:29:20 +0100

    removed unused kwargs

--- src/env/dm_control/dm_control/suite/common/settings.py
+++ src/env/dm_control/dm_control/suite/common/settings.py
@@ -25,35 +25,6 @@ def get_model_and_assets_from_setting_kwargs(model_fname, setting_kwargs=None):
     materials = xmltodict.parse(assets['./common/materials.xml'])
     skybox = xmltodict.parse(assets['./common/skybox.xml'])
 
-    # Edit lighting
-    if 'light_pos' in setting_kwargs:
-        assert isinstance(setting_kwargs['light_pos'], (list, tuple, np.ndarray))
-        light_pos = f'{setting_kwargs["light_pos"][0]} {setting_kwargs["light_pos"][1]} {setting_kwargs["light_pos"][2]}'
-        if 'light' in model['mujoco']['worldbody']:
-            model['mujoco']['worldbody']['light']['@pos'] = light_pos
-        elif 'light' in model['mujoco']['worldbody']['body']:
-            model['mujoco']['worldbody']['body']['light']['@pos'] = light_pos
-        else:
-            raise NotImplementedError('model xml does not contain entity light')
-
-    # Edit camera
-    if 'cam_pos' in setting_kwargs:
-        assert isinstance(setting_kwargs['cam_pos'], (list, tuple, np.ndarray))
-        cam_pos = f'{setting_kwargs["cam_pos"][0]} {setting_kwargs["cam_pos"][1]} {setting_kwargs["cam_pos"][2]}'
-        if 'camera' in model['mujoco']['worldbody']:
-            model['mujoco']['worldbody']['camera'][0]['@pos'] = cam_pos
-        elif 'camera' in model['mujoco']['worldbody']['body']:
-            model['mujoco']['worldbody']['body']['camera'][0]['@pos'] = cam_pos
-        else:
-            raise NotImplementedError('model xml does not contain entity camera')
-
-    # Edit distractor
-    if 'distractor_pos' in setting_kwargs:
-        assert isinstance(setting_kwargs['distractor_pos'], (list, tuple, np.ndarray))
-        distractor_pos = f'{setting_kwargs["distractor_pos"][0]} {setting_kwargs["distractor_pos"][1]} {setting_kwargs["distractor_pos"][2]}'
-        assert model['mujoco']['worldbody']['body'][-1]['@name'] == 'distractor', 'distractor must be in worldbody'
-        model['mujoco']['worldbody']['body'][-1]['geom']['@pos'] = distractor_pos
-
     # Edit grid floor
     if 'grid_rgb1' in setting_kwargs:
         assert isinstance(setting_kwargs['grid_rgb1'], (list, tuple, np.ndarray))
@@ -63,13 +34,6 @@ def get_model_and_assets_from_setting_kwargs(model_fname, setting_kwargs=None):
         assert isinstance(setting_kwargs['grid_rgb2'], (list, tuple, np.ndarray))
         materials['mujoco']['asset']['texture']['@rgb2'] = \
             f'{setting_kwargs["grid_rgb2"][0]} {setting_kwargs["grid_rgb2"][1]} {setting_kwargs["grid_rgb2"][2]}'
-    if 'grid_texrepeat' in setting_kwargs:
-        assert isinstance(setting_kwargs['grid_texrepeat'], (list, tuple, np.ndarray))
-        materials['mujoco']['asset']['material'][0]['@texrepeat'] = \
-            f'{setting_kwargs["grid_texrepeat"][0]} {setting_kwargs["grid_texrepeat"][1]}'
-    if 'grid_reflectance' in setting_kwargs:
-        materials['mujoco']['asset']['material'][0]['@reflectance'] = \
-            str(setting_kwargs["grid_reflectance"])
 
     # Edit self
     if 'self_rgb' in setting_kwargs:
